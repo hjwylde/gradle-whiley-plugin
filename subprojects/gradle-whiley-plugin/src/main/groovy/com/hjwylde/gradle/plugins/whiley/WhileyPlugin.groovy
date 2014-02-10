d438ae564477dc2285f3ec0d5e279de15a187327
Henry J. Wylde <accounts+git@hjwylde.com>
2014-02-10 13:46:18 +1300

    Added in requirement for a WHILEY_HOME environment variable

--- subprojects/gradle-whiley-plugin/src/main/groovy/com/hjwylde/gradle/plugins/whiley/WhileyPlugin.groovy
+++ subprojects/gradle-whiley-plugin/src/main/groovy/com/hjwylde/gradle/plugins/whiley/WhileyPlugin.groovy
@@ -2,6 +2,7 @@ package com.hjwylde.gradle.plugins.whiley
 
 import static org.gradle.api.tasks.SourceSet.MAIN_SOURCE_SET_NAME
 
+import org.gradle.api.InvalidUserDataException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPlugin
@@ -22,6 +23,8 @@ class WhileyPlugin implements Plugin<Project> {
      */
     @Override
     void apply(Project project) {
+        checkEnvironment()
+
         this.project = project
 
         project.plugins.apply(JavaPlugin.class)
@@ -29,7 +32,12 @@ class WhileyPlugin implements Plugin<Project> {
         configureTasks()
     }
 
-    void configureTasks() {
+    private void checkEnvironment() {
+        if (!System.env.WHILEY_HOME)
+            throw new InvalidUserDataException('Environment variable WHILEY_HOME is not set')
+    }
+
+    private void configureTasks() {
         project.sourceSets.all { set ->
             def name = set.name == MAIN_SOURCE_SET_NAME ? '' : set.name
             def taskName = 'compile' + name.capitalize() + 'Whiley'
