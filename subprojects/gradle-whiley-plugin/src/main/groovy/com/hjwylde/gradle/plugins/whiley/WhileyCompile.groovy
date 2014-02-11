e7248e172708c471b87730beb63e699640c73f86
Henry J. Wylde <accounts+git@hjwylde.com>
2014-02-11 16:01:07 +1300

    Added in a fix for the relative path compilation issue
    Forces all source files to be within a folder matching 'src/*/whiley'
    
    Disallows any source file from declaring a top level package of 'src'

--- subprojects/gradle-whiley-plugin/src/main/groovy/com/hjwylde/gradle/plugins/whiley/WhileyCompile.groovy
+++ subprojects/gradle-whiley-plugin/src/main/groovy/com/hjwylde/gradle/plugins/whiley/WhileyCompile.groovy
@@ -21,6 +21,9 @@ class WhileyCompile extends SourceTask {
 
         logger.info "Executing '{}'", (["${getWhileyBinDir()}/wyjc", '-cd', destinationDir] + files)
 
+        // Problem here with how the whiley compiler places the files in the build directory:
+        // Currently they are placed in their entire path relative from the working directory,
+        // unlike Java where it places it in a path based only on the package delcaration
         def proc = (["${getWhileyBinDir()}/wyjc", '-cd', destinationDir] + files).execute()
         proc.in.eachLine { if (it) logger.info it }
         proc.waitFor()
@@ -34,11 +37,38 @@ class WhileyCompile extends SourceTask {
             throw new InvalidUserDataException(sb.toString())
         }
 
-        didWork = proc.exitValue() == 0
+        // Hacky fix for the problem explained above...
+        // This forces all source files to be located within a path matching 'src/*/whiley'
+        // and for no file to have a top level package declaration of 'src'
+        def result = project.copy {
+            from("$destinationDir/src/$destinationDir.name/whiley") {
+                include '**/*.class'
+            }
+            into destinationDir
+        }
+
+        if (!result) {
+            throw new InvalidUserDataException("Unable to move source files from " +
+                    "'${project.relativePath(destinationDir)}/src/*/whiley' to " +
+                    "'${project.relativePath(destinationDir)}'")
+        }
+
+        // Delete the src directory
+        result = project.delete("$destinationDir/src")
+
+        if (!result) {
+            logger.warn "Unable to delete directory '{}/src'", project.relativePath(destinationDir)
+        }
+
+        didWork = !source.isEmpty()
+    }
+
+    protected String getWhileyDir() {
+        System.env.WHILEY_HOME
     }
 
     protected String getWhileyBinDir() {
-        "$System.env.WHILEY_HOME/bin"
+        "${getWhileyDir()}/bin"
     }
 }
 
